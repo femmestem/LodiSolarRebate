6d5c94ceea2cd768426341a91d74db65b2f39cac
Christine L. Feaster <feasterc@gmail.com>
2015-12-16 16:24:36 -0800

    Fix flash error messages, add styles

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,12 +3,12 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:error] = nil
-      flash.now[:notice] = "Thank you! A licensed contractor will contact you for your free rebate evaluation."
-      redirect_to '/'
+      flash[:error] = nil
+      flash[:notice] = "Thank you! A licensed contractor will contact you for your free rebate evaluation."
+      redirect_to root_url
     else
-      flash.now[:error] = "Cannot send message."
-      render :new
+      flash.now[:error] = "Message not sent. Please check for errors and submit again."
+      render 'application/index'
     end
   end
 end
